783955ca6d8b19cabcf40e2aac7032030ea11889
DAVID-ПК\DAVID <jakobian72780160@gmail.com>
2017-04-17 17:15:01 +0300

    Вводит числа в текстовое поле, без операций

--- js.js
+++ js.js
@@ -3,3 +3,12 @@
 function onPush(){
 	alert('Съебал');
 }
+ one.onclick = function() {
+    field.value+=one.value;
+};
+function NumInput(num){
+	field.value+= num.value;
+}
+function Operator(op){
+	field.value+= op.value;
+}
