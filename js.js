da9f868a98f54097b34445df0ae8fd5effb49268
DAVID-ПК\DAVID <jakobian72780160@gmail.com>
2017-04-20 17:01:26 +0300

    добавил к твоей функции foreach

--- js.js
+++ js.js
@@ -1,20 +1,34 @@
 ﻿
 "use strict"
-
+var elements = [];
+var str = "";
 function onPush(){
 	alert('Съебал');
 }
 one.onclick = function() {
 	field.value+=one.value;
 };
-function NumInput(num){
-	var str;
-	var arr1 = [];
-	var arr = [];
-	arr.push(+num);
-	field.value = arr;
-	
-	
+function buttonPress(value) {
+    if (typeof value === "number") {
+        if (elements.length === 0) {
+            elements.push(value);
+        } else {
+            var index = elements.length - 1;
+            var lastElement = elements[index];
+            if (typeof lastElement === "number") {
+                elements[index] = parseInt(elements[index].toString() + value.toString());
+            } else {
+                elements.push(value);
+            }
+        }
+    } else if (typeof value === "string" && elements.length > 0) {
+        elements.push(value);
+        
+    }
+elements.forEach(function(item) {
+  str += item;
+  });
+field.value = str;
 }
 function Operator(){
 	field.value+= minys.value;
@@ -23,7 +37,7 @@ function Operat(){
 	field.value+= plus.value;
 }
 function Ravno(){
-	field.value = field.value;
+	
 }
 function e(){
 	field.value = "";
